08b8bffd8761d2e5f08a25e125f4cf259831f43b
Nirdesh25 <nirdeshgothania25@gmail.com>
2024-05-25 05:09:30 +0530

    moreUpdatedServerURL

--- lib/sessionConstants.dart
+++ lib/sessionConstants.dart
@@ -1,5 +1,5 @@
 class sessionConstants {
   static String? email;
   static String? email2;
-  static const String host = 'https://proj-server.vercel.app/';
+  static const String host = 'https://proj-server.vercel.app';
 }
