d2e69100fe9c313f20010f39f5c0620075e588a9
Nirdesh25 <nirdeshgothania25@gmail.com>
2024-05-29 02:34:56 +0530

    changed server host

--- lib/sessionConstants.dart
+++ lib/sessionConstants.dart
@@ -1,6 +1,6 @@
 class SessionConstants {
   static String? email;
   static String? email2;
-  static const String host = 'http://localhost:3000';
+  static const String host = 'https://proj-server.onrender.com';
   static bool isContestActive = false;
 }
